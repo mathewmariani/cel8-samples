3dc947e0664e06328169ab7a9d98b1a406df3533
mathewmariani <someone.mariani@gmail.com>
2025-02-26 18:07:20 -0500

    Fixed samples/glitch

--- samples/glitch.c
+++ samples/glitch.c
@@ -5,7 +5,6 @@
 #include "platform-sokol.c"
 
 /* std */
-#include <stdio.h>
 #include <stdlib.h>
 
 void c8_load(void)
@@ -23,9 +22,9 @@ void c8_load(void)
 
 void c8_update(void)
 {
-    int8_t n = rand() % (0x10 * 0x10);
-    c8_poke(C8_MEM_VRAM_ADDR + (n * 2) + 0, rand() % 0xff);
-    c8_poke(C8_MEM_VRAM_ADDR + (n * 2) + 1, rand() % 0x7f);
+    uint8_t n = (uint8_t)(rand() % 256);
+    c8_poke(C8_MEM_VRAM_ADDR + (n * 2) + 0, rand() % 0xff); // color
+    c8_poke(C8_MEM_VRAM_ADDR + (n * 2) + 1, rand() % 0x7f); // character
 }
 
 void c8_draw(void)
